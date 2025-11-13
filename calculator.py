be228bf47820809084fa6f5d0abb270ac5c3f5b7
Jackson Tacca <Jtacca@ufl.edu>
2025-11-13 13:52:25 -0500

    modified calculator p1

---
+++ calculator.py
@@ -0,0 +1,14 @@
+import math
+
+def add(a, b): a + b
+
+def subtract(a, b): a - b
+
+def multiply(a, b): a * b
+
+def divide(a, b): b / a   # raise ZeroDivisionError if a == 0
+
+def logarithm(a, b): loga(b)# use math library/raise ValueError
+
+def exponent(a, b): ab
+
