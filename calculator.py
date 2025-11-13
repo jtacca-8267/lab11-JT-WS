936bbd7a9beeb58f15aa881f1f882032dd88d960
Jackson Tacca <Jtacca@ufl.edu>
2025-11-13 14:07:45 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -1,5 +1,13 @@
 import math
 
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of a negative number")
+    return math.sqrt(a)
+
 def add(a, b): a + b
 
 def subtract(a, b): a - b
